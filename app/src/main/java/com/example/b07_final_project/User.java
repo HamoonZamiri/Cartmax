4e7e997ef3abb353a588560c485b8dc78fcd10ba
maazh10 <maaz.hashmi@mail.utoronto.ca>
2021-11-23 23:57:51 -0500

    changed username to email

--- app/src/main/java/com/example/b07_final_project/User.java
+++ app/src/main/java/com/example/b07_final_project/User.java
@@ -2,17 +2,17 @@ package com.example.b07_final_project;
 
 public class User {
     private String name;
-    private String username;
+    private String email;
     private String password;
 
-    public User(String username, String password, String name) {
-        this.username = username;
+    public User(String email, String password, String name) {
+        this.email = email;
         this.password = password;
         this.name = name;
     }
 
-    public String getUsername() {
-        return username;
+    public String getemail() {
+        return email;
     }
 
     public String getPassword() {
@@ -25,7 +25,7 @@ public class User {
 
     @Override
     public String toString() {
-        return name + ": (" + username + ", " + password + ")";
+        return name + ": (" + email + ", " + password + ")";
     }
 
     @Override
@@ -35,11 +35,11 @@ public class User {
         if(this.getClass() != obj.getClass())
             return false;
         User other = (User)obj;
-        return this.username.equals(other.username);
+        return this.email.equals(other.email);
     }
 
     @Override
     public int hashCode() {
-        return username.hashCode();
+        return email.hashCode();
     }
 }
