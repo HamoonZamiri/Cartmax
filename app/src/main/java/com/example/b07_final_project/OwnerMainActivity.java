f26a5c8c7f8d11b597f725cd3831f011d36bde7f
Yu Yang Chen <kennyyuyang.chen@mail.utoronto.ca>
2021-11-30 14:08:25 -0500

    Fixed owner push

--- app/src/main/java/com/example/b07_final_project/OwnerMainActivity.java
+++ app/src/main/java/com/example/b07_final_project/OwnerMainActivity.java
@@ -19,29 +19,19 @@ public class OwnerMainActivity extends AppCompatActivity implements View.OnClick
         Customer c = new Customer("lol@gmail.com", "kek", "LMAO"); //placeholder
         name.setText(c.getName());
 
-        Button newOrder = (Button) findViewById(R.id.newOrder);
-        newOrder.setOnClickListener(this);
-
         Button logout = (Button) findViewById(R.id.logout);
         logout.setOnClickListener(this);
-
-        Button myOrders = (Button) findViewById(R.id.myOrders);
-        myOrders.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.newOrder:
-                startActivity(new Intent(this, StoreListActivity.class));
-                break;
+
             case R.id.logout:
                 startActivity(new Intent(this, LoginActivity.class));
                 break;
-            case R.id.myOrders:
-                startActivity(new Intent(this, MyOrdersActivity.class));
-                break;
+
         }
     }
 
