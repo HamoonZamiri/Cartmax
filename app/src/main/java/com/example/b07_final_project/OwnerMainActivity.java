a7780012a2bc0754acd15ae1472ff53c9a78c240
Yu Yang Chen <kennyyuyang.chen@mail.utoronto.ca>
2021-11-30 13:51:30 -0500

    Added Owner Main Activity

---
+++ app/src/main/java/com/example/b07_final_project/OwnerMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.b07_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_main);
+    }
+}
