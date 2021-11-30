39d66c0acdda6e01d344d47a949c218a1ef5e505
Yu Yang Chen <kennyyuyang.chen@mail.utoronto.ca>
2021-11-30 14:03:15 -0500

    Updated Owner Main

--- app/src/main/java/com/example/b07_final_project/CustomerMainActivity.java
+++ app/src/main/java/com/example/b07_final_project/CustomerMainActivity.java
@@ -18,35 +18,12 @@ public class CustomerMainActivity extends AppCompatActivity implements View.OnCl
         TextView name = (TextView) findViewById(R.id.customerName);
         Customer c = new Customer("lol@gmail.com", "kek", "LMAO"); //placeholder
         name.setText(c.getName());
-
-        Button newOrder = (Button) findViewById(R.id.newOrder);
-        newOrder.setOnClickListener(this);
-
-        Button logout = (Button) findViewById(R.id.logout);
-        logout.setOnClickListener(this);
-
-        Button myOrders = (Button) findViewById(R.id.myOrders);
-        myOrders.setOnClickListener(this);
-
-        Button ownerMain = (Button) findViewById(R.id.button2);
-        ownerMain.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.newOrder:
-                startActivity(new Intent(this, StoreListActivity.class));
-                break;
-            case R.id.logout:
-                startActivity(new Intent(this, LoginActivity.class));
-                break;
-            case R.id.myOrders:
-                startActivity(new Intent(this, MyOrdersActivity.class));
-                break;
-            case R.id.button2:
-                startActivity(new Intent(this, OwnerMainActivity.class));
-                break;
+
         }
     }
 
